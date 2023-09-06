68591840bb62197add07f3c221fdfab89328c3c1
Natraj Padwani <nextronickk@gmail.com>
2023-09-06 13:03:00 +0800

    Adding things according to NEXTjs doc

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { useFileSystemPublicRoutes: false };
 
-module.exports = nextConfig
+module.exports = nextConfig;
